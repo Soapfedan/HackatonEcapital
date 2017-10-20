2ef71a75a8e5019b9e7a543bd651660a9f2ac8f8
Simox <piccio.94@hotmail.it>
2017-10-20 12:14:44 +0200

    aaa

--- src/database/ConnectionHandler.java
+++ src/database/ConnectionHandler.java
@@ -1,5 +1,15 @@
+
 package database;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionHandler {
-
+	
+	public void init() throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection connection = null;
+		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:SID","username","password");
+		connection.close();
+	}
 }
