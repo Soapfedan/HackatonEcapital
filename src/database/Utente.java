e3be6b9a488063483277f36946944080296a4ff2
Federico Simonetti <federicosimonetti94@gmail.com>
2017-10-20 13:25:24 +0200

    dd

--- src/database/Utente.java
+++ src/database/Utente.java
@@ -1,5 +1,8 @@
 package database;
 
 public class Utente {
-
+	private final String UTENTE = "username";
+	private final String NOME = "nome";
+	private final String COGNOME = "cognome";
+	private final String PASSWORD = "password";
 }
