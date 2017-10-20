cc5d93c9360ab02a5186f2291fbcfb081c5cd32e
matteo <matcap94@gmail.com>
2017-10-20 16:22:20 +0200

    dasdasd

--- src/plug/PlugData.java
+++ src/plug/PlugData.java
@@ -1,6 +1,7 @@
 package plug;
 
-import java.sql.Date;
+import java.util.Date;
+import java.sql.Time;
 
 public class PlugData {
 	public int id;
@@ -10,7 +11,13 @@ public class PlugData {
 	public PlugData(int id, int consumption, String date) {
 		this.id = id;
 		this.consumption = consumption;
-		this.time = Date.valueOf(date);
+		this.time = new Date();
+	}
+	
+	public PlugData(byte[] buffer) {
+		id = buffer[0] << 8 | buffer[1];
+		consumption = buffer[2] << 8 | buffer[3];
+		time = new Date();
 	}
 	
 	
