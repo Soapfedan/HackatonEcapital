09cb9ef09a922c4e7f037edc30ddc2b0341b0e1e
Enrico <Enrico@172.23.179.138>
2017-10-20 19:37:06 +0200

    bugfix

--- src/database/Categoria.java
+++ src/database/Categoria.java
@@ -32,13 +32,42 @@ public class Categoria {
 
 		} 
 	}
+	
+	//Update categoria (passare null a n se non modificare stringa, 0 a pr se non modificare priorita
+	public static void updateCategoria(int id, String n, int pr) {
+		if(n!=null || n.isEmpty()) updateNome(id,n);
+		if(pr!=0) updatePriorita(id,pr);
+	}
 
 	//Modifica priorita
-	public static void updatePriorita(int id, int pr) {
+	private static void updatePriorita(int id, int pr) {
 		Statement statement = null;
 
 		String alterTableSQL = "UPDATE "+TABLE_NAME+" SET "
 				+PRIORITA+" = "+pr
+				+" WHERE "+CAT_ID+" = "+id;
+
+		try {
+			
+			statement = ConnectionHandler.getConn().createStatement();
+
+			System.out.println(alterTableSQL);
+
+			// execute insert SQL statement
+			statement.executeUpdate(alterTableSQL);
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} 
+	}
+	
+	private static void updateNome(int id, String n) {
+		Statement statement = null;
+
+		String alterTableSQL = "UPDATE "+TABLE_NAME+" SET "
+				+NOME+" = '"+n+"'"
 				+" WHERE "+CAT_ID+" = "+id;
 
 		try {
